e624a861e256003ab4513af6848050b443112996
James-Alex Matthews <vamtiger@vamtiger.com>
2019-05-06 03:53:44 +0200

    vamtiger-node-typescript-commit

--- build/test-json-ld.js
+++ build/test-json-ld.js
@@ -1,2 +1,2 @@
-!function(){"use strict";const{stringify:t}=JSON,{head:n}=document,e=document.createElement("script");e.type="application/ld+json",e.dataset.jsonLd="test/json-ld.js",e.innerHTML=t({"@context":"http://schema.org/","@type":"Painting"}),n.appendChild(e)}();
+!function(){"use strict";const{stringify:t}=JSON,{head:n}=document,e=document.createElement("script");e.type="application/ld+json",e.dataset.jsonLd="https://unpkg.com/test-json-ld",e.innerHTML=t({"@context":"http://schema.org/","@type":"Painting"}),n.appendChild(e)}();
 //# sourceMappingURL=test-json-ld.js.map
