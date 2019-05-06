6ccf01fd827df1d75360c3cd80f32c2d1e77663f
James-Alex Matthews <vamtiger@vamtiger.com>
2019-05-06 03:41:42 +0200

    vamtiger-node-typescript-commit

---
+++ build/test-json-ld.js
@@ -0,0 +1,2 @@
+!function(){"use strict";const{stringify:t}=JSON,{head:n}=document,e=document.createElement("script");e.type="application/ld+json",e.dataset.jsonLd="test/json-ld.js",e.innerHTML=t({"@context":"http://schema.org/","@type":"Painting"}),n.appendChild(e)}();
+//# sourceMappingURL=test-json-ld.js.map
